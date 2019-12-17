5fca3308d049e93e393d73928929ce2e808bf108
alexcravalho <a.d.cravalho@gmail.com>
2019-12-16 20:42:09 -0800

    edited SearchBarA and B components to yield results based on first letter of first name;

--- client/src/components/SearchBarB.jsx
+++ client/src/components/SearchBarB.jsx
@@ -25,7 +25,7 @@ function SearchBarB(props) {
   const results = !searchTerm
   ? props.playerList
   : props.playerList.filter(player =>
-      player.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      (player.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()) && player.name[0].toLowerCase() === searchTerm[0].toLocaleLowerCase())
     );
 
   return(
