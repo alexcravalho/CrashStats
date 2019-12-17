dc72e056b133303a28b61e5a44d4d332375b5d1b
alexcravalho <a.d.cravalho@gmail.com>
2019-12-16 20:38:13 -0800

    added ListNameA & ListNameB components for refactor;

---
+++ client/src/components/ListNameB.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const ListNameB = (props) => {
+
+  const [xbox, setXbox] = useState(false);
+
+  const handleDelete = (player) => {
+    // var playerSelect = confirm(`Are you sure you want to remove ${props.player.name} from the trade?`)
+    // if (playerSelect) {
+      // alert(`${props.player.name} was removed from the trade`)
+      props.deleteBList(props.player);
+      setXbox(false);
+    // }
+  }
+
+  return(
+  <div className="player-name" onMouseEnter={() => { setXbox(true) }} onMouseLeave={() => { setXbox(false) }}>
+    {xbox && <div className="x" onClick={() => { handleDelete(props.player) }}>X</div>}
+    <div className="player-name-text" >{props.player.name}</div>
+  </div>
+  )
+};
+
+export default ListNameB;
