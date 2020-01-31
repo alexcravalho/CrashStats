ba86a8eb6847c122eeb4018754999e732990bc33
alexcravalho <a.d.cravalho@gmail.com>
2020-01-30 18:55:38 -0800

    removed body-parser middleware; utilized express.json function;

--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
 const compression = require('compression');
 const express = require('express');
 const app = express();
-const PORT = 443 || process.env.PORT;
-const bp = require('body-parser');
+const PORT = 7000 || process.env.PORT;
 const path = require('path');
-const db = require('../database/db.js')
+const db = require('../database/db.js');
 
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'dist')));
-app.use(bp.json());
+app.use(express.json());
 app.use(compression());
 
 app.get('/api/players', (req, res) => {
-  console.log("got request")
+  console.log("got request");
   db.player.find({})
   .then((data) => { res.send(data) })
   .catch((err) => { console.log(err) })
 })
-
 
 app.listen(PORT, () => {
   console.log(`Express server is running on port: ${PORT}`);
